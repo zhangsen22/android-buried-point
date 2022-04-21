fe91df26180cbe721dd722269cbb6cc03ec9d13f
zhangsen <zhangsen839705693@163.com>
2022-04-21 12:07:16 +0800

    1

--- SensorsAnalyticsSDK/src/main/java/com/sensorsdata/analytics/android/sdk/util/AopUtil.java
+++ SensorsAnalyticsSDK/src/main/java/com/sensorsdata/analytics/android/sdk/util/AopUtil.java
@@ -1,20 +1,3 @@
-/*
- * Created by wangzhuozhou on 2016/12/2.
- * Copyright 2015－2022 Sensors Data Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.sensorsdata.analytics.android.sdk.util;
 
 import android.app.Activity;
@@ -25,19 +8,9 @@ import android.content.pm.PackageManager;
 import android.os.Build;
 import android.text.TextUtils;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.ViewParent;
 import android.view.Window;
-import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.CheckedTextView;
 import android.widget.CompoundButton;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.RadioButton;
-import android.widget.TextView;
-import android.widget.ToggleButton;
-
 import com.sensorsdata.analytics.android.sdk.AopConstants;
 import com.sensorsdata.analytics.android.sdk.R;
 import com.sensorsdata.analytics.android.sdk.SALog;
